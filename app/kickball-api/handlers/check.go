cc89d7f7cc657cba4efadf4f837692bf799b82f8
awe <a@awews.com>
2021-03-23 14:30:12 -0700

    remove status code logging

--- app/kickball-api/handlers/check.go
+++ app/kickball-api/handlers/check.go
@@ -18,6 +18,5 @@ func (c check) readiness(ctx context.Context, w http.ResponseWriter, r *http.Req
 	}{
 		Status: "OK",
 	}
-	log.Println(r, status)
 	return web.Respond(ctx, w, status, http.StatusOK)
 }
