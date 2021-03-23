53a4c60b905f76d8b469f96934526017e53f5aed
awe <a@awews.com>
2021-03-23 16:35:27 -0700

    create global package level variables

---
+++ business/mid/metrics.go
@@ -0,0 +1,14 @@
+package mid
+
+import "expvar"
+
+// m contains the global program counters for the application.
+var m = struct {
+	gr  *expvar.Int
+	req *expvar.Int
+	err *expvar.Int
+}{
+	gr:  expvar.NewInt("goroutines"),
+	req: expvar.NewInt("requests"),
+	err: expvar.NewInt("errors"),
+}
