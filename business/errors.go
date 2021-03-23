3af785d9f84e6a3f8a1741dac94d6fc1b55c72fa
awe <a@awews.com>
2021-03-23 15:24:34 -0700

    pull trace id out of context of error

--- business/errors.go
+++ business/errors.go
@@ -8,22 +8,22 @@ import (
 	"github.com/dapperauteur/go-base-service/foundation/web"
 )
 
-// Errors ...
+// Errors handles errors coming out of the call chain. It detects normal
+// application errors which are used to respond to the client in a uniform way.
+// Unexpected errors (status >= 500) are logged.
 func Errors(log *log.Logger) web.Middleware {
 
+	// This is the actual middleware function to be executed.
 	m := func(handler web.Handler) web.Handler {
+
+		// Create the handler that will be attached in the middleware chain.
 		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
-			if err := handler(ctx, w, r); err != nil {
-				// HANDLE ERROR
-				log.Println(err)
 
-				// HANDLE ERROR RESPONSE
-
-				// SHUTDOWN SIGNAL?
-				// return err
-
-				// DECISION
-				return nil
+			// If the context is missing this value, request the service
+			// to be shutdown gracefully.
+			v, ok := ctx.Value(web.KeyValues).(*web.Values)
+			if !ok {
+				return web.NewShutdownError("web value missing from context")
 			}
 
 			return nil
