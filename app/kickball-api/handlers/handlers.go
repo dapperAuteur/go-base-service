31b86b132324595168b109e256cc0a82c9b73018
awe <a@awews.com>
2021-03-23 11:15:36 -0700

    refactor to give caller the option to decouple from concrete mux

--- app/kickball-api/handlers/handlers.go
+++ app/kickball-api/handlers/handlers.go
@@ -12,7 +12,7 @@ import (
 )
 
 // API constructs an http.Handler with all application routes defined.
-func API(build string, shutdown chan os.Signal, log *log.Logger) http.Handler {
+func API(build string, shutdown chan os.Signal, log *log.Logger) *httptreemux.ContextMux {
 
 	tm := httptreemux.NewContextMux()
 
