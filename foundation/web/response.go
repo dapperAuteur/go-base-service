f7f90206b1539755366bf7549acfd522607b4335
awe <a@awews.com>
2021-03-23 13:25:57 -0700

    add json encoding to response

--- foundation/web/response.go
+++ foundation/web/response.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 )
 
@@ -16,5 +17,30 @@ func Respond(ctx context.Context, w http.ResponseWriter, data interface{}, statu
 		return NewShutdownError("web value missing from context")
 	}
 	v.StatusCode = statusCode
+
+	// If there is nothing to marshal then set status code and return.
+	if statusCode == http.StatusNoContent {
+		w.WriteHeader(statusCode)
+		return nil
+	}
+
+	// Convert the response value to JSON.
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// Set the content type and headers once we know marshaling has succeeded.
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write the status code to the response.
+	w.WriteHeader(statusCode)
+
+	// Send the result back to the client.
+	if _, err := w.Write(jsonData); err != nil {
+		return err
+	}
+
+	return nil
 
 }
