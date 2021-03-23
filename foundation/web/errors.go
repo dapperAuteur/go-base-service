5b6824878c71ed37d612175f231bfe1f87086998
awe <a@awews.com>
2021-03-23 13:06:08 -0700

    add FieldError and ErrorResponse struct
    adds data validation at field level and for consistent responses

---
+++ foundation/web/errors.go
@@ -0,0 +1,13 @@
+package web
+
+// FieldError is used to indicate an error with a specific request field.
+type FieldError struct {
+	Field string `json:"field"`
+	Error string `json:"error"`
+}
+
+// ErrorResponse is the form used for API responses from failures in the API.
+type ErrorResponse struct {
+	Error  string       `json:"error"`
+	Fields []FieldError `json:"fields,omitempty"`
+}
