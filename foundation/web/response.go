72e0ce5d14897ef63cebba37a8bb8f53e5b7df47
awe <a@awews.com>
2021-03-23 13:25:08 -0700

    check that context is there, else shutdown

--- foundation/web/response.go
+++ foundation/web/response.go
@@ -8,4 +8,13 @@ import (
 // Respond converts a Go value to JSON and sends it to the client.
 func Respond(ctx context.Context, w http.ResponseWriter, data interface{}, statusCode int) error {
 
+	// Set the status code for the request logger middleware.
+	// If the context is missing this value, request the service
+	// to be shutdown gracefully.
+	v, ok := ctx.Value(KeyValues).(*Values)
+	if !ok {
+		return NewShutdownError("web value missing from context")
+	}
+	v.StatusCode = statusCode
+
 }
