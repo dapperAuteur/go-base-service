3ab8b20d73ece37b827eac6eb2825f7978d74be8
awe <a@awews.com>
2021-03-23 14:49:40 -0700

    add functionality to get unique id out of request.Context
    shutdown if not there

--- business/mid/logger.go
+++ business/mid/logger.go
@@ -4,22 +4,44 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/dapperauteur/go-base-service/foundation/web"
 )
 
-// Logger ...
+// Logger writes some information about the request to the logs in the
+// format: TraceID : (200) GET /foo -> IP ADDR (latency)
 func Logger(log *log.Logger) web.Middleware {
-	// readiness from check.go
+
+	// This is the actual middleware function to be executed.
 	m := func(before web.Handler) web.Handler {
 
+		// Create the handler that will be attached in the middleware chain.
 		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+
+			// BOILERPLATE - LOGGING
+			// If the context is missing this value, request the service
+			// to be shutdown gracefully.
+			v, ok := ctx.Value(web.KeyValues).(*web.Values)
+			if !ok {
+				return web.NewShutdownError("web value missing from context")
+			}
+
+			log.Printf("%s: started: %s %s -> %s",
+				v.TraceID,
+				r.Method, r.URL.Path, r.RemoteAddr,
+			)
+
 			// readiness from check.go
 			err := before(ctx, w, r)
 
-			// BOILERPLATE - LOGGING
-			log.Println(r)
+			log.Printf("%s: completed: %s %s -> %s (%d) (%s)",
+				v.TraceID,
+				r.Method, r.URL.Path, r.RemoteAddr,
+				v.StatusCode, time.Since(v.Now),
+			)
 
+			// Return the error so it can be handled further up the chain.
 			return err
 		}
 		return h
