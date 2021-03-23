d51d11f5350e5581faae8df24daecd2b68751659
awe <a@awews.com>
2021-03-23 11:25:25 -0700

    add logger by turning readiness to a method on check from a func

--- app/kickball-api/handlers/check.go
+++ app/kickball-api/handlers/check.go
@@ -2,14 +2,20 @@ package handlers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
-func readiness(w http.ResponseWriter, r *http.Request) {
+type check struct {
+	log *log.Logger
+}
+
+func (c check) readiness(w http.ResponseWriter, r *http.Request) {
 	status := struct {
 		Status string
 	}{
 		Status: "OK",
 	}
 	json.NewEncoder(w).Encode(status)
+	log.Println(r, status)
 }
