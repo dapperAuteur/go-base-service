c255060daed579cba950f541735fed29fe83d834
awe <a@awews.com>
2021-03-23 12:15:51 -0700

    add comments, shutdown channel, simple error handling for web app

--- foundation/web/web.go
+++ foundation/web/web.go
@@ -4,34 +4,51 @@ package web
 import (
 	"context"
 	"net/http"
+	"os"
+	"syscall"
 
 	"github.com/dimfeld/httptreemux/v5"
 )
 
-// Handler ...
+// A Handler is a type that handles an http request within our own little mini
+// framework.
 type Handler func(ctx context.Context, w http.ResponseWriter, r *http.Request) error
 
-// App ...
+// App is the entrypoint into our application and what configures our context
+// object for each of our http handlers. Feel free to add any configuration
+// data/logic on this App struct.
 type App struct {
 	*httptreemux.ContextMux
+	shutdown chan os.Signal
 }
 
-// NewApp ...
-func NewApp() *App {
+// NewApp creates an App value that handle a set of routes for the application.
+func NewApp(shutdown chan os.Signal) *App {
 	app := App{
 		ContextMux: httptreemux.NewContextMux(),
+		shutdown:   shutdown,
 	}
 	return &app
 }
 
+// SignalShutdown is used to gracefully shutdown the app when an integrity
+// issue is identified.
+func (a *App) SignalShutdown() {
+	a.shutdown <- syscall.SIGTERM
+}
+
 // Handle
-func (a *App) Handle(method string, path string, readiness Handler) {
+func (a *App) Handle(method string, path string, handler Handler) {
 
 	h := func(w http.ResponseWriter, r *http.Request) {
-		// r is the readiness function in check.go (check.readiness)
+
 		// BOILERPLATE
-		if err := readiness(r.Context(), w, r); err != nil {
-			// Handle error
+
+		// r is the readiness function in check.go (check.readiness)
+		if err := handler(r.Context(), w, r); err != nil {
+			// Handling error, can do more
+			a.SignalShutdown()
+			return
 		}
 
 		// BOILERPLATE
