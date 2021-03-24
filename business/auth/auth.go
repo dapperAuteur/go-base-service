92c7a5f5c67d205b67c0248111dacd312e7131c1
awe <a@awews.com>
2021-03-23 18:21:41 -0700

    add function to lookup public key

--- business/auth/auth.go
+++ business/auth/auth.go
@@ -2,6 +2,8 @@
 package auth
 
 import (
+	"crypto/rsa"
+
 	"github.com/dgrijalva/jwt-go/v4"
 )
 
@@ -23,8 +25,8 @@ type Claims struct {
 	Roles []string `json:"roles"`
 }
 
-// Authorized returns true if the claims has at least one of the provided roles.
-func (c Claims) Authorized(roles ...string) bool {
+// HasRole returns true if the claims has at least one of the provided roles.
+func (c Claims) HasRole(roles ...string) bool {
 	for _, has := range c.Roles {
 		for _, want := range roles {
 			if has == want {
@@ -34,3 +36,21 @@ func (c Claims) Authorized(roles ...string) bool {
 	}
 	return false
 }
+
+// Keys represents an in memory store of keys.
+type Keys map[string]*rsa.PrivateKey
+
+/*
+PublicKeyLookup defines the signature of a function to lookup public keys.
+
+In a production system, a key id (KID) is used to retrieve the correct public key to parse a JWT for auth and claims.
+A key lookup function is provided to perform the task of retrieving a KID for a given public key.
+
+A key lookup function is required for creating an Authenticator.
+
+ * Private keys should be rotated. During the transition period, tokens signed with the old and new keys can coexist by looking up the correct public key by KID.
+
+ * KID to public key resolution is usually accomplished via a public JWKS endpoint.
+ See https://auth0.com/docs/jwks for more details.
+*/
+type PublicKeyLookup func(kid string) (*rsa.PublicKey, err)
