1e553419f75e6316d72be7488faff641de98f84d
awe <a@awews.com>
2021-03-23 18:10:04 -0700

    create const for user roles and context key

---
+++ business/auth/auth.go
@@ -0,0 +1,24 @@
+// Package auth provides authentication and authorization support.
+package auth
+
+import (
+	"github.com/dgrijalva/jwt-go/v4"
+)
+
+// These are the expected values for Claims.Roles.
+const (
+	RoleAdmin = "ADMIN"
+	RoleUser  = "USER"
+)
+
+// ctxKey represents the type of value for the context key.
+type ctxKey int
+
+// Key is used to store/retrieve a Claims value from a context.Context.
+const Key ctxKey = 1
+
+// Claims represents the authorization claims transmitted via a JWT.
+type Claims struct {
+	jwt.StandardClaims
+	Roles []string `json:"roles"`
+}
