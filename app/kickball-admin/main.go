eef28d6dbec772251f514cc569677e55aab3ad59
awe <a@awews.com>
2021-03-23 17:21:31 -0700

    add process to generate private key

--- app/kickball-admin/main.go
+++ app/kickball-admin/main.go
@@ -1,10 +1,50 @@
 package main
 
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"log"
+	"os"
+)
+
 /*
 	openssl genpkey -algorithm RSA -out private.pem -pkeyopt rsa_keygen_bits:2048
 	openssl rsa -pubout -in private.pem -out public.pem
 */
 
 func main() {
+	keygen()
+
+}
+
+func keygen() {
+
+	// Generate a new private key.
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Create a file for the private key information in PEM form.
+	privateFile, err := os.Create("private.pem")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer privateFile.Close()
+
+	// Construct a PEM block for the private key.
+	privateBlock := pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+	}
+
+	// Write the private key to the private key file.
+	if err := pem.Encode(privateFile, &privateBlock); err != nil {
+		log.Fatalln(err)
+	}
+
+	// ================================
 
 }
