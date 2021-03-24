f5331c805d7908eed463ec4aefbb8d9ff28ed8a0
awe <a@awews.com>
2021-03-23 19:42:37 -0700

    add middleware to inject auth into app

---
+++ business/mid/auth.go
@@ -0,0 +1,31 @@
+package mid
+
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	"github.com/dapperauteur/go-base-service/business/auth"
+	"github.com/dapperauteur/go-base-service/foundation/web"
+)
+
+// ErrForbidden is returned when an authenticated user does not have a sufficient role for an action
+var ErrForbidden = web.NewRequestError(
+	errors.New("you are not authorized for that action"),
+	http.StatusForbidden,
+)
+
+// Authenticate validates a JWT from the `Authorization` header.
+func Authenticate(a *auth.Auth) web.Middleware {
+
+	// This is the actual middleware function to be executed.
+	m := func(handler web.Handler) web.Handler {
+
+		// Create the handler that will be attached in the middleware chain.
+		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+			return nil
+		}
+		return h
+	}
+	return m
+}
