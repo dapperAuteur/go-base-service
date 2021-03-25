5dd50cac2f8c2fd621e4d56419d3ee22cc714966
awe <a@awews.com>
2021-03-25 16:55:02 -0700

    on liveness check, get os.Hostname and return with structured data on app

--- app/service-api/handlers/check.go
+++ app/service-api/handlers/check.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/dapperauteur/go-base-service/foundation/web"
 )
@@ -32,4 +33,35 @@ func (c check) readiness(ctx context.Context, w http.ResponseWriter, r *http.Req
 		Status: "OK",
 	}
 	return web.Respond(ctx, w, status, http.StatusOK)
+}
+
+// liveness returns simple status info if the service is alive. If the
+// app is deployed to a Kubernetes cluster, it will also return pod, node, and
+// namespace details via the Downward API. The Kubernetes environment variables
+// need to be set within your Pod/Deployment manifest.
+func (c check) liveness(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+
+	host, err := os.Hostname()
+	if err != nil {
+		host = "unavailable"
+	}
+
+	info := struct {
+		Status    string `json:"status,omitempty"`
+		Build     string `json:"build,omitempty"`
+		Host      string `json:"host,omitempty"`
+		Pod       string `json:"pod,omitempty"`
+		PodIP     string `json:"podIP,omitempty"`
+		Node      string `json:"node,omitempty"`
+		Namespace string `json:"namespace,omitempty"`
+	}{
+		Status:    "up",
+		Build:     c.build,
+		Host:      host,
+		Pod:       os.Getenv("KUBERNETES_PODNAME"),
+		PodIP:     os.Getenv("KUBERNETES_NAMESPACE_POD_IP"),
+		Node:      os.Getenv("KUBERNETES_NODENAME"),
+		Namespace: os.Getenv("KUBERNETES_NAMESPACE"),
+	}
+	return web.Respond(ctx, w, info, http.StatusOK)
 }
