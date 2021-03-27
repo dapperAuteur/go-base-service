bea2ae0a79e7d243f435fca2b5d265834f9985f9
awe <a@awews.com>
2021-03-27 10:58:08 -0700

    add readiness support for db

--- foundation/database/database.go
+++ foundation/database/database.go
@@ -50,3 +50,30 @@ func Open(cfg Config) (*sqlx.DB, error) {
 
 	return sqlx.Open("postgres", u.String())
 }
+
+func StatusCheck(ctx context.Context, db *sqlx.DB) error {
+
+	// First check we can ping the database.
+	var pingError error
+	for attempts := 1; ; attempts++ {
+		pingError = db.Ping()
+		if pingError == nil {
+			break
+		}
+		time.Sleep(time.Duration(attempts) * 100 * time.Millisecond)
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
+	}
+
+	// Make sure we didn't timeout or be cancelled.
+	if ctx.Err() != nil {
+		return ctx.Err()
+	}
+
+	// Run a simple query to determine connectivity. Running this query forces a
+	// round trip through the database.
+	const q = `SELECT true`
+	var tmp bool
+	return db.QueryRowContext(ctx, q).Scan(&tmp)
+}
