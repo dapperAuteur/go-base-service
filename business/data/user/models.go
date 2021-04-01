fde7b2c47fbe66e236e6f987a66165177d0b0dba
awe <a@awews.com>
2021-04-01 16:41:58 -0700

    change spacing on comments

--- business/data/user/models.go
+++ business/data/user/models.go
@@ -26,11 +26,11 @@ type NewUser struct {
 	PasswordConfirm string   `json:"password_confirm" validate:"eqfield=Password"`
 }
 
-// UpdateUser defines what information may be provided to modify an existing
-// User. All fields are optional so clients can send just the fields they want
-// changed. It uses pointer fields so we can differentiate between a field that
-// was not provided and a field that was provided as explicitly blank. Normally
-// we do not want to use pointers to basic types but we make exceptions around
+// UpdateUser defines what information may be provided to modify an existing User.
+// All fields are optional so clients can send just the fields they want changed.
+// It uses pointer fields so we can differentiate between a field that
+// was not provided and a field that was provided as explicitly blank.
+// Normally we do not want to use pointers to basic types but we make exceptions around
 // marshalling/unmarshalling.
 type UpdateUser struct {
 	Name            *string  `json:"name"`
