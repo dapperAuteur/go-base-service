c93ea90769bb3aeea6894bf0559e8f1009a7b676
awe <a@awews.com>
2021-04-01 12:47:25 -0700

    add user error vars, user struct, new func and create method

--- business/data/user/user.go
+++ business/data/user/user.go
@@ -1,2 +1,42 @@
 // Package user contains user related CRUD functionality.
 package user
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// Set of error variables for CRUD operations.
+var (
+	ErrNotFound              = errors.New("not found")
+	ErrInvalidID             = errors.New("ID is not in its proper form")
+	ErrAuthenticationFailure = errors.New("authentication failed")
+	ErrForbidden             = errors.New("attempted action is not allowed")
+)
+
+// User manages the set of API's for user access.
+type User struct {
+	log *log.Logger
+	db  *sqlx.DB
+}
+
+// New constructs a user for api access.
+func New(log *log.Logger, db *sqlx.DB) User {
+	return User{
+		log: log,
+		db:  db,
+	}
+}
+
+// Create inserts a new user into the database.
+func (u User) Create(ctx context.Context, traceID string, nu NewUser, now time.Time) (Info, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(nu.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return Info{}, errors.Wrap(err, "generating password hash")
+	}
+}
