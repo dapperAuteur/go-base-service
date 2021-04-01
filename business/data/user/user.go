d7a8379a997c58e19961a6a5d713b5b1ca1f9fc0
awe <a@awews.com>
2021-04-01 13:51:19 -0700

    add User CRUD

--- business/data/user/user.go
+++ business/data/user/user.go
@@ -3,9 +3,14 @@ package user
 
 import (
 	"context"
+	"database/sql"
 	"log"
 	"time"
 
+	"github.com/dapperauteur/go-base-service/business/auth"
+	"github.com/dapperauteur/go-base-service/foundation/database"
+
+	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
 	"golang.org/x/crypto/bcrypt"
@@ -39,4 +44,191 @@ func (u User) Create(ctx context.Context, traceID string, nu NewUser, now time.T
 	if err != nil {
 		return Info{}, errors.Wrap(err, "generating password hash")
 	}
+
+	usr := Info{
+		ID:           uuid.New().String(),
+		Name:         nu.Name,
+		Email:        nu.Email,
+		PasswordHash: hash,
+		Roles:        nu.Roles,
+		DateCreated:  now.UTC(),
+		DateUpdated:  now.UTC(),
+	}
+
+	const q = `
+	INSERT INTO users
+		(user_id, name, email, password_hash, roles, date_created, date_updated)
+	VALUES
+		($1, $2, $3, $4, $5, $6, $7)`
+
+	u.log.Printf("%s : %s : query : %s", traceID, "user.Create",
+		database.Log(q, usr.ID, usr.Name, usr.Email, usr.PasswordHash, usr.Roles, usr.DateCreated, usr.DateUpdated),
+	)
+
+	if _, err := u.db.ExecContext(ctx, q, usr.ID, usr.Name, usr.Email, usr.PasswordHash, usr.Roles, usr.DateCreated, usr.DateUpdated); err != nil {
+		return Info{}, errors.Wrap(err, "inserting user")
+	}
+
+	return usr, nil
+}
+
+// Update replaces a user document in the database.
+func (u User) Update(ctx context.Context, traceID string, claims auth.Claims, userID string, uu UpdateUser, now time.Time) error {
+
+	usr, err := u.QueryByID(ctx, traceID, claims, userID)
+	if err != nil {
+		return errors.Wrap(err, "updating user")
+	}
+
+	if uu.Name != nil {
+		usr.Name = *uu.Name
+	}
+	if uu.Email != nil {
+		usr.Email = *uu.Email
+	}
+	if uu.Roles != nil {
+		usr.Roles = uu.Roles
+	}
+	if uu.Password != nil {
+		pw, err := bcrypt.GenerateFromPassword([]byte(*uu.Password), bcrypt.DefaultCost)
+		if err != nil {
+			return errors.Wrap(err, "generating password hash")
+		}
+		usr.PasswordHash = pw
+	}
+	usr.DateUpdated = now
+
+	const q = `
+	UPDATE
+		users
+	SET 
+		"name" = $2,
+		"email" = $3,
+		"roles" = $4,
+		"password_hash" = $5,
+		"date_updated" = $6
+	WHERE
+		user_id = $1`
+
+	u.log.Printf("%s : %s : query : %s", traceID, "user.Update",
+		database.Log(q, usr),
+	)
+
+	if _, err := u.db.ExecContext(ctx, q, usr.ID, usr.Name, usr.Email, usr.PasswordHash, usr.Roles, usr.DateUpdated); err != nil {
+		return errors.Wrapf(err, "updating user %s", usr.ID)
+	}
+
+	return nil
+}
+
+// Delete removes a user from the database.
+func (u User) Delete(ctx context.Context, traceID string, claims auth.Claims, userID string) error {
+	if _, err := uuid.Parse(userID); err != nil {
+		return ErrInvalidID
+	}
+
+	const q = `
+	DELETE FROM
+		users
+	WHERE
+		user_id = :user_id`
+
+	u.log.Printf("%s: %s : query : %s", traceID, "user.Delete",
+		database.Log(q, userID),
+	)
+
+	if _, err := u.db.ExecContext(ctx, q, userID); err != nil {
+		return errors.Wrapf(err, "deleting user %s", userID)
+	}
+
+	return nil
+}
+
+// Query retrieves a list of existing users from the database.
+func (u User) Query(ctx context.Context, traceID string, pageNumber int, rowsPerPage int) ([]Info, error) {
+	const q = `
+	SELECT
+		*
+	FROM
+		users
+	ORDER BY
+		user_id
+	OFFSET :offset ROWS FETCH NEXT :rows_per_page ROWS ONLY`
+
+	u.log.Printf("%s: %s: %s", traceID, "user.Query",
+		database.Log(q),
+	)
+
+	users := []Info{}
+	if err := u.db.SelectContext(ctx, &users, q); err != nil {
+		return nil, errors.Wrap(err, "selecting users")
+	}
+
+	return users, nil
+}
+
+// QueryByID gets the specified user from the database.
+func (u User) QueryByID(ctx context.Context, traceID string, claims auth.Claims, userID string) (Info, error) {
+	if _, err := uuid.Parse(userID); err != nil {
+		return Info{}, ErrInvalidID
+	}
+
+	// If you are not an admin and looking to retrieve someone other than yourself.
+	if !claims.Authorize(auth.RoleAdmin) && claims.Subject != userID {
+		return Info{}, ErrForbidden
+	}
+
+	const q = `
+	SELECT
+		*
+	FROM
+		users
+	WHERE 
+		user_id = $1`
+
+	u.log.Printf("%s: %s: %s", traceID, "user.QueryByID",
+		database.Log(q, userID),
+	)
+
+	var usr Info
+	if err := u.db.GetContext(ctx, &usr, q, userID); err != nil {
+		if err == sql.ErrNoRows {
+			return Info{}, ErrNotFound
+		}
+		return Info{}, errors.Wrapf(err, "selecting user %q", userID)
+	}
+
+	return usr, nil
+
+}
+
+// QueryByEmail gets the specified user from the database by email.
+func (u User) QueryByEmail(ctx context.Context, traceID string, claims auth.Claims, email string) (Info, error) {
+
+	const q = `
+	SELECT
+		*
+	FROM
+		users
+	WHERE
+		email = :email`
+
+	u.log.Printf("%s: %s: %s", traceID, "user.QueryByEmail",
+		database.Log(q, email),
+	)
+
+	var usr Info
+	if err := u.db.GetContext(ctx, &usr, q, email); err != nil {
+		if err == sql.ErrNoRows {
+			return Info{}, ErrNotFound
+		}
+		return Info{}, errors.Wrapf(err, "selecting user %q", email)
+	}
+
+	// If you are not an admin and looking to retrieve someone other than yourself.
+	if !claims.Authorize(auth.RoleAdmin) && claims.Subject != usr.ID {
+		return Info{}, ErrForbidden
+	}
+
+	return usr, nil
 }
