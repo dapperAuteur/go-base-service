37a565d74ceae9c63a5e6d4e51f3539a652f2e25
awe <a@awews.com>
2021-04-01 14:57:58 -0700

    add new unit test with command to start docker testing container

--- business/tests/tests.go
+++ business/tests/tests.go
@@ -1,2 +1,32 @@
 // Package tests contains supporting code for running tests.
 package tests
+
+import (
+	"log"
+	"testing"
+
+	"github.com/jmoiron/sqlx"
+)
+
+// Success and failure markers.
+const (
+	Success = "\u2713"
+	Failed  = "\u2717"
+)
+
+// Configuration for running tests.
+var (
+	dbImage = "postgres:13-alpine"
+	dbPort  = "5432"
+	dbArgs  = []string{"-e", "POSTGRES_PASSWORD=postgres"}
+	AdminID = "5cf37266-3473-8f07-a58d4a30fa2f"
+	UserID  = "45b5fbd3-755f-4379-8f07-a58d4a30fa2f"
+)
+
+// NewUnit creates a test database inside a Docker container.
+// It creates the required table structure but the database is otherwise empty.
+// It returns the database to use as well as a function to call at the end of the test.
+func NewUnit(t *testing.T) (*log.Logger, *sqlx.DB, func()) {
+
+	c := startContainer(t, dbImage, dbPort, dbArgs...)
+}
