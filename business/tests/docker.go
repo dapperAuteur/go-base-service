af181dca6b47befb76b74e3e0c060e455ea902ae
awe <a@awews.com>
2021-04-01 14:17:05 -0700

    add docker start command for tests

--- business/tests/docker.go
+++ business/tests/docker.go
@@ -1,1 +1,55 @@
 package tests
+
+import (
+	"bytes"
+	"encoding/json"
+	"net"
+	"os/exec"
+	"testing"
+)
+
+// Container tracks information about the docker container started for tests.
+type Container struct {
+	ID   string
+	Host string // IP:Port
+}
+
+func startContainer(t *testing.T, image string, port string, args ...string) *Container {
+	arg := []string{"run", "-P", "-d"}
+	arg = append(arg, args...)
+	arg = append(arg, image)
+
+	cmd := exec.Command("docker", arg...)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	if err := cmd.Run(); err != nil {
+		t.Fatalf("could not start container %s: %v", image, err)
+	}
+
+	id := out.String()[:12]
+
+	cmd = exec.Command("docker", "inspect", id)
+	out.Reset()
+	cmd.Stdout = &out
+	if err := cmd.Run(); err != nil {
+		t.Fatalf("could not inspect container %s: %v", id, err)
+	}
+
+	var doc []map[string]interface{}
+	if err := json.Unmarshal(out.Bytes(), &doc); err != nil {
+		t.Fatalf("could not decode json: %v", err)
+	}
+
+	ip, randPort := extractIPPort(t, doc, port)
+
+	c := Container{
+		ID:   id,
+		Host: net.JoinHostPort(ip, randPort),
+	}
+
+	t.Logf("Image:       %s", image)
+	t.Logf("ContainerID: %s", c.ID)
+	t.Logf("Host:        %s", c.Host)
+
+	return &c
+}
