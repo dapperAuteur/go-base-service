4f65209280a44adbf57a06b4dc48fe95a9dc0c6b
awe <a@awews.com>
2021-04-01 15:59:42 -0700

    add update test, fix import name, reuse claims

--- business/data/user/user_test.go
+++ business/data/user/user_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/dapperauteur/go-base-service/business/data/user"
 	"github.com/dapperauteur/go-base-service/business/tests"
 	"github.com/dgrijalva/jwt-go"
-	"github.com/google/go-cmp"
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestUser(t *testing.T) {
@@ -19,43 +19,88 @@ func TestUser(t *testing.T) {
 
 	t.Log("Given the need to work with User records.")
 	{
-		ctx := tests.Context()
-		now := time.Date(2018, time.October, 1, 0, 0, 0, 0, time.UTC)
-		traceID := "00000000-0000-0000-0000-000000000000"
+		testID := 0
+		t.Logf("\tTest %d:\tWhen handling a single User.", testID)
+		{
+			ctx := tests.Context()
+			now := time.Date(2018, time.October, 1, 0, 0, 0, 0, time.UTC)
+			traceID := "00000000-0000-0000-0000-000000000000"
 
-		nu := user.NewUser{
-			Name:            "awe ful",
-			Email:           "aweful@awews.com",
-			Roles:           []string{auth.RoleAdmin},
-			Password:        "spread love",
-			PasswordConfirm: "spread love",
-		}
+			nu := user.NewUser{
+				Name:            "awe ful",
+				Email:           "aweful@awews.com",
+				Roles:           []string{auth.RoleAdmin},
+				Password:        "spread love",
+				PasswordConfirm: "spread love",
+			}
 
-		usr, err := u.Create(ctx, traceID, nu, now)
-		if err != nil {
-			t.Fatalf("\t%s\tTest %d:\tShould be able to create user : %s.", tests.Failed, testID, err)
-		}
-		t.Logf("\t%s\tTest %d:\tShould be able to create user.", tests.Success, testID)
+			usr, err := u.Create(ctx, traceID, nu, now)
+			if err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to create user : %s.", tests.Failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to create user.", tests.Success, testID)
 
-		claims := auth.Claims{
-			StandardClaims: jwt.StandardClaims{
-				Issuer:    "service project",
-				Subject:   usr.ID,
-				ExpiresAt: now.Add(time.Hour).Unix(),
-				IssuedAt:  now.Unix(),
-			},
-			Roles: []string{auth.RoleUser},
-		}
+			claims := auth.Claims{
+				StandardClaims: jwt.StandardClaims{
+					Issuer:    "service project",
+					Subject:   usr.ID,
+					ExpiresAt: now.Add(time.Hour).Unix(),
+					IssuedAt:  now.Unix(),
+				},
+				Roles: []string{auth.RoleUser},
+			}
 
-		saved, err := u.QueryByID(ctx, traceID, claims, usr.ID)
-		if err != nil {
-			t.Fatalf("\t%s\tTest %d:\tShould be able to retrieve user by ID: %s.", tests.Failed, testID, err)
-		}
-		t.Logf("\t%s\tTest %d:\tShould be able to retrieve user by ID.", tests.Success, testID)
+			saved, err := u.QueryByID(ctx, traceID, claims, usr.ID)
+			if err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to retrieve user by ID: %s.", tests.Failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to retrieve user by ID.", tests.Success, testID)
 
-		if diff := cmp.Diff(usr, saved); diff != "" {
-			t.Fatalf("\t%s\tTest %d:\tShould get back the same user. Diff:\n%s", tests.Failed, testID, diff)
+			if diff := cmp.Diff(usr, saved); diff != "" {
+				t.Fatalf("\t%s\tTest %d:\tShould get back the same user. Diff:\n%s", tests.Failed, testID, diff)
+			}
+			t.Logf("\t%s\tTest %d:\tShould get back the same user.", tests.Success, testID)
+
+			upd := user.UpdateUser{
+				Name:  tests.StringPointer("Anthony McDonald"),
+				Email: tests.StringPointer("a@awews.com"),
+			}
+
+			claims = auth.Claims{
+				StandardClaims: jwt.StandardClaims{
+					Issuer:    "service project",
+					ExpiresAt: now.Add(time.Hour).Unix(),
+					IssuedAt:  now.Unix(),
+				},
+				Roles: []string{auth.RoleAdmin},
+			}
+
+			if err := u.Update(ctx, traceID, claims, usr.ID, upd, now); err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to update user : %s.", tests.Failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to update user.", tests.Success, testID)
+
+			saved, err = u.QueryByEmail(ctx, traceID, claims, *upd.Email)
+			if err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to retrieve user by Email : %s.", tests.Failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to retrieve user by Email : %s.", tests.Success, testID)
+
+			if saved.Name != *upd.Name {
+				t.Errorf("\t%s\tTest %d:\tShould be able to see updates to Name.", tests.Failed, testID)
+				t.Logf("\t\tTest %d:\tGot: %v", testID, saved.Name)
+				t.Logf("\t\tTest %d:\tExp: %v", testID, *upd.Name)
+			} else {
+				t.Logf("\t%s\tTest %d:\tShould be able to see updates to Name.", tests.Success, testID)
+			}
+
+			if saved.Email != *upd.Email {
+				t.Errorf("\t%s\tTest %d:\tShould be able to see updates to Email.", tests.Failed, testID)
+				t.Logf("\t\tTest %d:\tGot: %v", testID, saved.Email)
+				t.Logf("\t\tTest %d:\tExp: %v", testID, *upd.Email)
+			} else {
+				t.Logf("\t%s\tTest %d:\tShould be able to see updates to Email.", tests.Success, testID)
+			}
 		}
-		t.Logf("\t%s\tTest %d:\tShould get back the same user.", tests.Success, testID)
 	}
 }
