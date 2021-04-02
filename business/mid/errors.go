11c7a4fc39bb613ac73aa0915d32f75104d0d404
awe <a@awews.com>
2021-04-02 16:07:12 -0700

    add tracing to logger and errors middleware

--- business/mid/errors.go
+++ business/mid/errors.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/dapperauteur/go-base-service/foundation/web"
+	"go.opentelemetry.io/otel/trace"
 )
 
 // Errors handles errors coming out of the call chain. It detects normal
@@ -18,6 +19,8 @@ func Errors(log *log.Logger) web.Middleware {
 
 		// Create the handler that will be attached in the middleware chain.
 		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+			ctx, span := trace.SpanFromContext(ctx).Tracer().Start(ctx, "business.mid.errors")
+			defer span.End()
 
 			// If the context is missing this value, request the service
 			// to be shutdown gracefully.
--- business/mid/logger.go
+++ business/mid/logger.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/dapperauteur/go-base-service/foundation/web"
+	"go.opentelemetry.io/otel/trace"
 )
 
 // Logger writes some information about the request to the logs in the
@@ -18,6 +19,9 @@ func Logger(log *log.Logger) web.Middleware {
 
 		// Create the handler that will be attached in the middleware chain.
 		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+
+			ctx, span := trace.SpanFromContext(ctx).Tracer().Start(ctx, "business.mid.logger")
+			defer span.End()
 
 			// BOILERPLATE - LOGGING
 			// If the context is missing this value, request the service
