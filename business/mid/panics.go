9a1bca389323efc135f74d5aa072837e088abc5d
awe <a@awews.com>
2021-04-02 16:08:30 -0700

    add tracing to panics middleware

--- business/mid/panics.go
+++ business/mid/panics.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/dapperauteur/go-base-service/foundation/web"
 	"github.com/pkg/errors"
+	"go.opentelemetry.io/otel/trace"
 )
 
 // Panics recovers from panics and converts the panic to an error so it is
@@ -19,6 +20,9 @@ func Panics(log *log.Logger) web.Middleware {
 
 		// Create the handler that will be attached in the middleware chain.
 		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) (err error) {
+
+			ctx, span := trace.SpanFromContext(ctx).Tracer().Start(ctx, "business.mid.panics")
+			defer span.End()
 
 			// If the context is missing this value, request the service
 			// to be shutdown gracefully.
