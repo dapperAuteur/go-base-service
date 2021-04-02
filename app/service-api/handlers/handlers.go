11979aa649c4540a55f0a904dd38cd46c5c2e6b6
awe <a@awews.com>
2021-04-01 18:35:48 -0700

    remove versioning

--- app/service-api/handlers/handlers.go
+++ app/service-api/handlers/handlers.go
@@ -30,14 +30,14 @@ func API(build string, shutdown chan os.Signal, log *log.Logger, a *auth.Auth, d
 
 	// Register user management and authentication endpoints.
 	ug := userGroup{
-		store: user.New(log, db),
-		auth:  a,
+		user: user.New(log, db),
+		auth: a,
 	}
-	app.Handle(http.MethodGet, "/v1/users/:page/:rows", ug.query, mid.Authenticate(a), mid.Authorize(log, auth.RoleAdmin))
-	app.Handle(http.MethodGet, "/v1/users/token/:kid", ug.token)
-	app.Handle(http.MethodGet, "/v1/users/:id", ug.queryByID, mid.Authenticate(a))
-	app.Handle(http.MethodPost, "/v1/users", ug.create, mid.Authenticate(a), mid.Authorize(log, auth.RoleAdmin))
-	app.Handle(http.MethodPut, "/v1/users/:id", ug.update, mid.Authenticate(a), mid.Authorize(log, auth.RoleAdmin))
-	app.Handle(http.MethodDelete, "/v1/users/:id", ug.delete, mid.Authenticate(a), mid.Authorize(log, auth.RoleAdmin))
+	app.Handle(http.MethodGet, "/users/:page/:rows", ug.query, mid.Authenticate(a), mid.Authorize(log, auth.RoleAdmin))
+	app.Handle(http.MethodGet, "/users/token/:kid", ug.token)
+	app.Handle(http.MethodGet, "/users/:id", ug.queryByID, mid.Authenticate(a))
+	app.Handle(http.MethodPost, "/users", ug.create, mid.Authenticate(a), mid.Authorize(log, auth.RoleAdmin))
+	app.Handle(http.MethodPut, "/users/:id", ug.update, mid.Authenticate(a), mid.Authorize(log, auth.RoleAdmin))
+	app.Handle(http.MethodDelete, "/users/:id", ug.delete, mid.Authenticate(a), mid.Authorize(log, auth.RoleAdmin))
 	return app
 }
