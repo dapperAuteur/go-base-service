a46578c78537fdaa84e2bcdbc688c9340111287e
awe <a@awews.com>
2021-04-02 15:50:58 -0700

    add tracing provider

--- app/service-api/main.go
+++ app/service-api/main.go
@@ -20,6 +20,9 @@ import (
 	"github.com/dapperauteur/go-base-service/foundation/database"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/pkg/errors"
+	"go.opentelemetry.io/otel/api/global"
+	"go.opentelemetry.io/otel/exporters/trace/zipkin"
+	"go.opentelemetry.io/otel/sdk/trace"
 )
 
 /*
@@ -72,6 +75,11 @@ func run(log *log.Logger) error {
 			Host       string `conf:"default:db"`
 			Name       string `conf:"default:postgres"`
 			DisableTLS bool   `conf:"default:true"`
+		}
+		Zipkin struct {
+			ReporterURI string  `conf:"default:http://zipkin:9411/api/v2/spans"`
+			ServiceName string  `conf:"default:service-api"`
+			Probability float64 `conf:"default:0.05"`
 		}
 	}
 
@@ -160,6 +168,35 @@ func run(log *log.Logger) error {
 		log.Printf("main: Database Stopping: %s", cfg.DB.Host)
 		db.Close()
 	}()
+
+	// =========================================================================
+	// Start Tracing Support
+
+	// WARNING: The current Init settings are using defaults which may not be
+	// compatible with your project. Please review the documentation for
+	// opentelemetry.
+
+	log.Println("main: Initializing OT/Zipkin tracing support")
+
+	exporter, err := zipkin.NewRawExporter(
+		cfg.Zipkin.ReporterURI,
+		cfg.Zipkin.ServiceName,
+		zipkin.WithLogger(log),
+	)
+	if err != nil {
+		return errors.Wrap(err, "creating new exporter")
+	}
+
+	tp := trace.NewTracerProvider(
+		trace.Withconfig(trace.Config{DefaultSampler: trace.TraceIDRatioBased(cfg.Zipkin.Probability)}),
+		trace.WithBatcher(exporter,
+			trace.WithMaxExportBatchSize(trace.DefaultMaxExportBatchSize),
+			trace.WithBatchTimeout(trace.DefaultBatchTimeout),
+			trace.WithMaxExportBatchSize(trace.DefaultMaxExportBatchSize),
+		),
+	)
+
+	global.SetTracerProvider(tp)
 
 	// =========================================================================
 	// Start Debug Service
