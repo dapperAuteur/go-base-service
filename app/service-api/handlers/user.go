abc2e9431723c887cb9bd6359d213bbb312c8854
awe <a@awews.com>
2021-04-01 19:14:06 -0700

    add create user handler

--- app/service-api/handlers/user.go
+++ app/service-api/handlers/user.go
@@ -40,3 +40,52 @@ func (ug userGroup) query(ctx context.Context, w http.ResponseWriter, r *http.Re
 
 	return web.Respond(ctx, w, users, http.StatusOK)
 }
+
+func (ug userGroup) queryByID(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+
+	v, ok := ctx.Value(web.KeyValues).(*web.Values)
+	if !ok {
+		return web.NewShutdownError("web value missing from context")
+	}
+
+	claims, ok := ctx.Value(auth.Key).(auth.Claims)
+	if !ok {
+		return errors.New("claims missing from context")
+	}
+
+	params := web.Params(r)
+	usr, err := ug.user.QueryByID(ctx, v.TraceID, claims, params["id"])
+	if err != nil {
+		switch err {
+		case user.ErrInvalidID:
+			return web.NewRequestError(err, http.StatusBadRequest)
+		case user.ErrNotFound:
+			return web.NewRequestError(err, http.StatusNotFound)
+		case user.ErrForbidden:
+			return web.NewRequestError(err, http.StatusForbidden)
+		default:
+			return errors.Wrapf(err, "ID: %s", params["id"])
+		}
+	}
+
+	return web.Respond(ctx, w, usr, http.StatusOK)
+}
+
+func (ug userGroup) create(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	v, ok := ctx.Value(web.KeyValues).(*web.Values)
+	if !ok {
+		return web.NewShutdownError("web value missing from context")
+	}
+
+	var nu user.NewUser
+	if err := web.Decode(r, &nu); err != nil {
+		return errors.Wrap(err, "unable to decode payload")
+	}
+
+	usr, err := ug.user.Create(ctx, v.TraceID, nu, v.Now)
+	if err != nil {
+		return errors.Wrapf(err, "User: %+v", &usr)
+	}
+
+	return web.Respond(ctx, w, usr, http.StatusCreated)
+}
